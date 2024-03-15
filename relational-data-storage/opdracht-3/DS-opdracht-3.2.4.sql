d62b3d2c0eb4cfd3c067b22240b355789c4e076f
MilanLucas <159128273+MilanLucas@users.noreply.github.com>
2024-03-15 09:39:59 +0100

    Create DS-opdracht-3.2.4.sql

---
+++ relational-data-storage/opdracht-3/DS-opdracht-3.2.4.sql
@@ -0,0 +1,5 @@
+SELECT a.name, a.straat, a.huisnr, a.postcode
+FROM mhl_suppliers a
+JOIN mhl_cities b ON a.city_ID = b.id
+JOIN mhl_cities c ON a.p_city_ID = c.id
+WHERE b.name = 'amsterdam' AND c.name = 'den haag';
