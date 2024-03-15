832f93a8ab90ab351de2ee470e28e692f90277d2
MilanLucas <159128273+MilanLucas@users.noreply.github.com>
2024-03-15 09:08:26 +0100

    Update DS-opdracht-3.2.1.sql

--- relational-data-storage/opdracht-3/DS-opdracht-3.2.1.sql
+++ relational-data-storage/opdracht-3/DS-opdracht-3.2.1.sql
@@ -1,1 +1,1 @@
-SELECT b.name, b.straat, b.huisnr, b.postcode FROM `mhl_cities` a LEFT JOIN `mhl_suppliers` b on a.id=b.city_id WHERE a.name='amsterdam'; 
+SELECT b.name, b.straat, b.huisnr, b.postcode FROM `mhl_cities` a INNER JOIN `mhl_suppliers` b on a.id=b.city_id WHERE a.name='amsterdam'; 
