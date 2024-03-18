c2b7a37d7e034096619f6b96b095c3be55d21529
MilanLucas <159128273+MilanLucas@users.noreply.github.com>
2024-03-18 10:24:52 +0100

    Update DS-opdracht-4.1.7.sql

--- relational-data-storage/opdracht-4/DS-opdracht-4.1.7.sql
+++ relational-data-storage/opdracht-4/DS-opdracht-4.1.7.sql
@@ -1,3 +1,3 @@
-SELECT a.name, b.name, a.id, b.id, a.commune_ID FROM mhl_cities a 
-INNER JOIN mhl_communes b ON a.commune_ID = b.id
-ORDER BY a.name;
+SELECT * FROM mhl_cities a 
+INNER JOIN mhl_communes b ON a.commune_ID = b.id 
+WHERE a.name = b.name; 
