db4d7f77307297edfe7e301b16e99e67f9658f4d
MilanLucas <159128273+MilanLucas@users.noreply.github.com>
2024-03-25 11:29:21 +0100

    Create DS-opdracht-7.sql

---
+++ mysql-advanced/DS-opdracht-7.sql
@@ -0,0 +1,29 @@
+CREATE TRIGGER employee_aur
+AFTER UPDATE
+ON employees FOR EACH ROW
+BEGIN 
+    IF OLD.date_fired IS NULL AND NEW.date_fired IS NOT NULL THEN
+    INSERT INTO employeearchive(employeenumber, date_fired) 
+    VALUES(OLD.employeenumber, NEW.date_fired);
+    END IF;
+END;
+
+DELIMITER $$
+
+CREATE FUNCTION fired_check(
+    status TIMESTAMP
+)
+RETURNS VARCHAR(5)
+DETERMINSTIC
+BEGIN
+    DECLARE status_fired VARCHAR(5)
+    IF status IS NOT NULL THEN
+        SET status_fired = "FIRED";
+    ELSEIF status IS NULL THEN
+        SET status_fired = "";
+    END IF;
+    RETURN (status_fired);
+END$$
+
+DELIMITER ; 
+
